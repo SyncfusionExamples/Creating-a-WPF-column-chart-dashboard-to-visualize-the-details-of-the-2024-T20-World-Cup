f32695aea72a85be08ecf6a1cde1ac1a818d71aa
saiyathalifathimabee.m@syncfusion.com <Saiyathalif@17>
2024-07-19 18:41:42 +0530

    added the sample

---
+++ T20Match2024/2024_T20_Match/2024_T20_Match/Model/T20_Model.cs
@@ -0,0 +1,23 @@
+﻿namespace _2024_T20_Match
+{
+    public class PlayersData
+    {
+        public string? Name { get; set; }
+        public int Score { get; set; }
+    }
+
+    public class CountryData
+    {
+        public int Play { get; set; }
+        public int Wins { get; set; }
+        public string? Runs { get; set; }
+        public int CatchValue { get; set; }
+        public int Score6s { get; set; }
+        public int Score4s { get; set; }
+        public int Boundaries { get; set; }
+        public int Batter { get; set; }
+        public string? BatterImage { get; set; }
+        public int WicketTaker { get; set; }
+        public string? WicketTakerImage { get; set; }
+    }
+}
